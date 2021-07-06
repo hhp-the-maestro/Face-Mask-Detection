d15b04403d740d50914a597af1f55c68d39fc9e9
Kali <hhp07022000@gmail.com>
2021-07-06 09:09:37 -0400

    mask detection rebuilt

--- mask_detector.py
+++ mask_detector.py
@@ -1,101 +1,63 @@
-import os
 import cv2
-import time
-import imutils
 import numpy as np
+import time
 from imutils.video import VideoStream
+import mediapipe as mp
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
+from face_detection.face_detection_module import FaceDetector
 
+face_detector = FaceDetector()
+capture = cv2.VideoCapture('./Pose estimation/videos/dance.mp4')
+# capture = VideoStream(0).start()
+mask_net = load_model('mask_detector.model')
+p_time = 0
 
-def detect_and_predict_mask(frame, facenet, masknet):
-    # grab the dimensions of the frame and then construct a blob
-    # from i
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-    (104.0, 177.0, 123.0))
-    # pass the blob through the network and obtain the face detections
-    facenet.setInput(blob)
-    detections = facenet.forward()
-    print(detections.shape)
-    # initialize our list of faces, their corresponding locations,
-    # and the list of predictions from our face mask network
+def utils(img, bboxes):
     faces = []
-    locs = []
-    preds = []
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the detection
-        confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > 0.5:
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype('int')
-            # ensure the bounding boxes fall within the dimensions of
-            # the frame
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-            # extract the face ROI, convert it from BGR to RGB channel
-            # ordering, resize it to 224x224, and preprocess it
-            face = frame[startY:endY, startX:endX]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            face = cv2.resize(face, (224, 224))
-            face = img_to_array(face)
-            face = preprocess_input(face)
+    for bbox in bboxes:
+        start_x, start_y, end_x, end_y = bbox
+        end_x += start_x
+        end_y += start_y
+        face = img[int(start_y):int(end_y), int(start_x): int(end_x)]
+        face = cv2.resize(img, (224, 224))
+        face = img_to_array(face)
+        face = preprocess_input(face)
+        prediction = model([face])
+        preds = np. argmax(prediction)
+        if preds == 1:
+            color = (0, 0, 255)
+            text = 'no mask'
+        else:
+            color = (0, 255, 0)
+            text = 'mask'
+        cv2.rectangle(img, bbox, color, 1)
+        cv2.putText(img, text, (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
 
-            # add the face and bounding boxes to their respective
-            # lists
-            faces.append(face)
-            locs.append((startX, startY, endX, endY))
-    # make a prediction if and only if one face is detcted
-    if len(faces) > 0:
-        faces = np.array(faces, dtype='float32')
-        preds = masknet.predict(faces, batch_size=32)
-
-    return (locs, preds)
+    return img
 
 
-prototxtPath = r"deploy.prototxt"
-weightsPath = r"res10_300x300_ssd_iter_140000.caffemodel"
-faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-maskNet = load_model('mask_detector.model')
+def model(face):
+    if len(face) > 0:
+        face = np.array(face, dtype=np.float32)
+        predictions = mask_net.predict(face, batch_size=32)
+        return predictions
 
-camera = VideoStream(0).start()
+
 
 while True:
+    _, img = capture.read()
+    d_img, bboxes = face_detector.find_faces(img, draw=False, score=False)
+    faces = utils(img, bboxes)
 
-    frame = camera.read()
-   # frame = cv2.resize(frame, (700, 500))
-    frame = imutils.resize(frame, width=700)
+    c_time = time.time()
+    fps = str(int(1/(c_time - p_time)))
+    p_time = c_time
 
-    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+    cv2.putText(img, fps, (70, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2)
+    cv2.imshow('live', img)
+    cv2.waitKey(20)
 
-    for (box, pred) in zip(locs, preds):
 
-        (start_x, start_y, end_x, end_y) = box
-        (mask, withoutMask) = pred
-
-        label = "Mask" if mask > withoutMask else "No Mask"
-        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-        # include the probability in the label
-        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-        # display the label and bounding box rectangle on the output
-        # frame
-        cv2.putText(frame, label, (start_x, start_y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-        cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), color, 2)
-
-    cv2.imshow('Frame', frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord('q'):
-        break
-
-cv2.destroyAllWindows()
-camera.stop()
